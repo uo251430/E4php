ea67196a279aa5014ce9182755f89713fb3452cd
uo251430 <90914876+uo251430@users.noreply.github.com>
2021-12-13 19:57:20 +0100

    Create Ejercicio4.php

---
+++ Ejercicio4.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es"><head>
+    <meta charset="UTF-8">
+    <title>Gas Natural</title>
+    <link rel="stylesheet" type="text/css" href="Ejercicio4.css">
+</head>
+<body>
+    <section>
+        <h1>Obtención precio gas natural con la API <a href="https://www.alphavantage.co/">alphavantage</a> </h1>
+        <h2>JSON decodificado correctamente</h2><h3>El precio del Gas Natural es: 26.6500 UDM </h3></section></body></html>
